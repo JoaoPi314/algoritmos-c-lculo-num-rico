99c22790fe7477abb9f8a6719d3dc35511352eb3
Vinícius de França Nóbrega <43653890+Viniciusfn@users.noreply.github.com>
2019-10-25 00:14:00 -0300

    Expansão para Gauss-Jordan
    Cálculo de Solução e Inversa

--- gaus.m
+++ gaus.m
@@ -1,9 +1,9 @@
-%método da eliminaçao de Gauss
+%mÃ©todo da eliminaÃ§ao de Gauss
 
 clear all; clc; clear;
 
 
-n = input('Insira o numero de incógnitas do seu sistema: ');
+n = input('Insira o numero de incÃ³gnitas do seu sistema: ');
 
 %A = [0.8 -0.2 -0.2 -0.3; -0.2 0.9 -0.2 -0.3;
 %     -0.3 -0.3 0.8 -0.2; -0.2 -0.2 -0.4 0.8];
@@ -26,32 +26,73 @@ disp('Digite os coeficientes do sistema');
 m = 0;
 
 for c2 = 1:n
-  fprintf('Equação %d\n', c2);
+  fprintf('EquaÃ§Ã£o %d\n', c2);
   for c3 = 1:n
     A(c2,c3) = input('');
   endfor
   B(c2) = input('');
 endfor
 %Escalonamento
-for j = 1:n-1
-  for i = j+1:n
-    m = A(i, j)/A(j, j);
-    B(i) = B(i) - (m*B(j));
-    for k = j+1:n
-      A(i, k) = A(i, k) - m*A(j, k); 
-    endfor
+%for j = 1:n-1
+%  for i = j+1:n
+%    m = A(i, j)/A(j, j);
+%    B(i) = B(i) - (m*B(j));
+%    for k = j+1:n
+%      A(i, k) = A(i, k) - m*A(j, k); 
+%    endfor
+%  endfor
+%endfor
+
+%ajustes de nomenclatura
+tam = n
+
+%Escalonamento de GAUSS-JORDAN
+for posPivo = 1:1:tam
+  pivo = A(posPivo, posPivo)
+  if pivo == 0 #Contornar isso
+    fprintf('Erro\n');
+    break;
+  end
+  for lin = 1:1:tam
+    if(!(lin == posPivo))
+      mult = A(lin,posPivo)/pivo
+      for col = 1:1:tam
+        A(lin,col) = A(lin,col) - mult*(A(posPivo,col));
+        Inv(lin,col) = Inv(lin,col) - mult*(Inv(posPivo,col));
+      endfor
+      B(lin,1) = B(lin,1) - mult*B(posPivo,1);
+    endif
   endfor
 endfor
 
-X = 0:1:n-1;
-X = X.';
 
-for i = n:-1:1
-  X(i) = B(i)/A(i, i);
-  for j = i+1:n
-    X(i) -= (A(i,j)*X(j))/A(i,i);
+%X = 0:1:n-1;
+%X = X.';
+
+%for i = n:-1:1
+%  X(i) = B(i)/A(i, i);
+%  for j = i+1:n
+%    X(i) -= (A(i,j)*X(j))/A(i,i);
+%  endfor
+%endfor
+
+%X
+
+
+% Obtendo resultados
+for lin = 1:1:tam
+  B(lin,1) = B(lin,1)/A(lin,lin);
+  for i = 1:1:tam
+    Inv(lin,i) = Inv(lin,i)/A(lin,lin);
   endfor
+  A(lin,lin) = 1;
 endfor
 
-X
 
+% Mostrando resultados
+fprintf("Matriz:\n");
+A #mostra como estahÂ¡ a matriz
+fprintf("Matriz Inversa:\n");
+Inv
+fprintf("SoluÃ§Ã£o:\n");
+B
